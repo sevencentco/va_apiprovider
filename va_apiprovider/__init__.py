ea5ad3e23c46ddd0624dc37bac695602a22b231a
Your Name <you@example.com>
2025-11-01 05:39:05 +0700

    [update][BluePrint]: add api by function & fix bug Duplicate route names

--- va_apiprovider/__init__.py
+++ va_apiprovider/__init__.py
@@ -3,3 +3,4 @@ __version__ = '0.1.0'
 # make the following names available as part of the public API
 from .core import APIProvider
 from .core import IllegalArgumentError
+from .func import api_provider
--- va_apiprovider/core.py
+++ va_apiprovider/core.py
@@ -1,14 +1,11 @@
 from collections import defaultdict
 from collections import namedtuple
 from sanic import Blueprint
+from sanic import Blueprint, response
 
 from .views import ModelView
 
 class IllegalArgumentError(Exception):
-    """This exception is raised when a calling function has provided illegal
-    arguments to a function or method.
-
-    """
     pass
 
 #: The set of methods which are allowed by default when creating an API
@@ -31,7 +28,6 @@ class APIProvider(object):
         if not existing:
             next_number = 0
         else:
-            # for brevity
             b = basename
             existing_numbers = [int(n.partition(b)[-1]) for n in existing]
             next_number = max(existing_numbers) + 1
@@ -114,53 +110,39 @@ class APIProvider(object):
                             results_per_page=results_per_page, max_results_per_page=max_results_per_page, \
                             preprocess=preprocessors_, postprocess=postprocessors_, primary_key=primary_key,\
                             db=restapi_ext.db)
-                               
-        blueprintname = APIProvider._next_blueprint_name(app.blueprints, apiname)
+                         
+        blueprintname = APIProvider._next_blueprint_name(app.blueprints, apiname) 
+        bp_route_name = blueprintname + "_nim" #### no_instance_methods
         blueprint = Blueprint(blueprintname, url_prefix=url_prefix)
-        blueprint.add_route(api_view,collection_endpoint,methods=no_instance_methods)
-        
+        blueprint.add_route(handler=api_view, uri=collection_endpoint,
+                methods=no_instance_methods, name=bp_route_name,)
+ 
         #DELETE, GET, PUT
+        bp_route_name = blueprintname + "_im" #### instance_methods
         instance_endpoint = '{0}/<instid>'.format(collection_endpoint)
-        blueprint.add_route(api_view,instance_endpoint,methods=instance_methods)
+        blueprint.add_route(handler=api_view, uri=instance_endpoint,
+                methods=instance_methods, name=bp_route_name,)
         
         return blueprint
     
     def create_api(self, *args, **kw):
-        # Check if the user is providing a specific Flask application with
-        # which the model's API will be associated.
         if 'app' in kw:
-            # If an application object was already provided in the constructor,
-            # raise an error indicating that the user is being confusing.
             if self.app is not None:
                 msg = ('Cannot provide a application in the APIProvider'
                        ' constructor and in create_api(); must choose exactly'
                        ' one')
                 raise IllegalArgumentError(msg)
             app = kw.pop('app')
-            # If the Flask application has already been initialized, then
-            # immediately create the API blueprint.
-            #
-            # TODO This is something of a fragile check for whether or not
-            # init_app() has been called on kw['app'], since some other
-            # (malicious) code could simply add the key 'restless' to the
-            # extensions dictionary.
             if self.name in app.ctx.extensions:
                 blueprint = self.create_api_blueprint(app=app, *args, **kw)
                 app.blueprint(blueprint)
-            # If the Flask application has not yet been initialized, then stash
-            # the positional and keyword arguments for later initialization.
             else:
                 self.apis_to_create[app].append((args, kw))
-        # The user did not provide a Flask application here.
         else:
-            # If a Flask application object was already provided in the
-            # constructor, immediately create the API blueprint.
             if self.app is not None:
                 app = self.app
                 blueprint = self.create_api_blueprint(app=app, *args, **kw)
                 app.blueprint(blueprint)
-            # If no Flask application was provided in the constructor either,
-            # then stash the positional and keyword arguments for later
-            # initalization.
             else:
                 self.apis_to_create[None].append((args, kw))
+                
